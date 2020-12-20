e965e9a7d3bc1d03c19ae74ee134093786cb9e43
mahatodaya <mahatodayaram1@gmail.com>
2020-12-20 10:37:51 +0545

    Commit for Lab 5 exercise:

---
+++ app/src/main/java/lab05/AlertDialogActivity.kt
@@ -0,0 +1,17 @@
+package lab05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+import com.example.lab01.R
+
+class AlertDialogActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alert_dialog)
+
+
+    }
+}
