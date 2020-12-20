4c1eb233090e0f3bfacdaa3d0cd68a0a8aa691d3
mahatodaya <mahatodayaram1@gmail.com>
2020-12-20 10:42:02 +0545

    Binding was done.

--- app/src/main/java/lab05/AlertDialogActivity.kt
+++ app/src/main/java/lab05/AlertDialogActivity.kt
@@ -6,12 +6,37 @@ import android.widget.*
 import com.example.lab01.R
 
 class AlertDialogActivity : AppCompatActivity() {
+    private lateinit var etFullName : EditText
+    private lateinit var etEmail : EditText
+    private lateinit var rdoGroup : RadioGroup
+    private lateinit var rdoStudent : RadioButton
+    private lateinit var rdoStaff : RadioButton
+    private lateinit var llStudent: LinearLayout
+    private lateinit var batchSpinner: Spinner
+    private lateinit var etStudentID: EditText
+    private lateinit var llStaff: LinearLayout
+    private lateinit var departmentSpinner: Spinner
+    private lateinit var etPassword: EditText
+    private lateinit var etConfirmPassword: EditText
+    private lateinit var btnRegister: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alert_dialog)
 
-
+        etFullName = findViewById(R.id.etFullName)
+        etEmail = findViewById(R.id.etEmail)
+        rdoGroup = findViewById(R.id.rdoStudent_StaffGroup)
+        rdoStudent = findViewById(R.id.rdoStudent)
+        rdoStaff = findViewById(R.id.rdoStaff)
+        llStudent = findViewById(R.id.llStudent)
+        batchSpinner = findViewById(R.id.batchSpinner)
+        etStudentID = findViewById(R.id.etStudentID)
+        llStaff = findViewById(R.id.llStaff)
+        departmentSpinner = findViewById(R.id.departmentSpinner)
+        etPassword = findViewById(R.id.etPassword)
+        etConfirmPassword = findViewById(R.id.etConfirmPassword)
+        btnRegister = findViewById(R.id.btnRegister)
     }
 }
