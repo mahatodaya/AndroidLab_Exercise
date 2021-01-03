4a365cd13d4872f350d958e3f0099f0ebef42946
mahatodaya <mahatodayaram1@gmail.com>
2021-01-03 12:59:22 +0545

    1. Binding in both activity was done.

--- app/src/main/java/lab07/Lab07FormActivity.kt
+++ app/src/main/java/lab07/Lab07FormActivity.kt
@@ -2,11 +2,31 @@ package lab07
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import com.example.lab01.R
 
 class Lab07FormActivity : AppCompatActivity() {
+    private lateinit var etName : EditText
+    private lateinit var etAddress : EditText
+    private lateinit var etMobile : EditText
+    private lateinit var rdoGender: RadioGroup
+    private lateinit var rdoMale : RadioButton
+    private lateinit var rdoFemale : RadioButton
+    private lateinit var btnSubmit : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lab07_form)
+
+        etName = findViewById(R.id.etName)
+        etAddress = findViewById(R.id.etAddress)
+        etMobile = findViewById(R.id.etMobile)
+        rdoGender = findViewById(R.id.rdoGroup)
+        rdoMale = findViewById(R.id.rdoMale)
+        rdoFemale = findViewById(R.id.rdoFemale)
+        btnSubmit = findViewById(R.id.btnSubmit)
     }
 }
--- app/src/main/java/lab07/Lab07OutputActivity.kt
+++ app/src/main/java/lab07/Lab07OutputActivity.kt
@@ -2,11 +2,29 @@ package lab07
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
 import com.example.lab01.R
 
 class Lab07OutputActivity : AppCompatActivity() {
+
+    private lateinit var lvStudent : ListView
+    private lateinit var tvName : TextView
+    private lateinit var tvAddress : TextView
+    private lateinit var tvGender : TextView
+    private lateinit var tvMobileNumber : TextView
+    private lateinit var btnStudent: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lab07__output)
+
+        lvStudent = findViewById(R.id.lvStudent)
+        tvName = findViewById(R.id.tvName)
+        tvAddress = findViewById(R.id.tvAddress)
+        tvGender = findViewById(R.id.tvGender)
+        tvMobileNumber = findViewById(R.id.tvMobileNumber)
+        btnStudent = findViewById(R.id.btnAddStudent)
     }
 }
