e9270787f9fc6de158f0ff3d8d590c6be22c859c
mahatodaya <mahatodayaram1@gmail.com>
2021-01-03 09:39:47 +0545

    Commit for Lab 7 exercise :

---
+++ app/src/main/java/lab07/Lab07OutputActivity.kt
@@ -0,0 +1,12 @@
+package lab07
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lab01.R
+
+class Lab07OutputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lab07__output)
+    }
+}
