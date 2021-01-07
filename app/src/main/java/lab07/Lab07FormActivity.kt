c4a1d8d99055ca994b69569805736c29a7478fd1
mahatodaya <mahatodayaram1@gmail.com>
2021-01-07 23:11:04 +0545

    4. New Student class was added in students list and tested by printing it in logcat.

--- app/src/main/java/lab07/Lab07FormActivity.kt
+++ app/src/main/java/lab07/Lab07FormActivity.kt
@@ -1,12 +1,15 @@
 package lab07
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import com.example.lab01.R
+import kotlin.math.log
 
 class Lab07FormActivity : AppCompatActivity() {
     private lateinit var etName : EditText
@@ -14,6 +17,8 @@ class Lab07FormActivity : AppCompatActivity() {
     private lateinit var etMobile : EditText
     private lateinit var rdoGender: RadioGroup
     private lateinit var btnSubmit : Button
+
+    private val students = arrayListOf<Student>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +42,11 @@ class Lab07FormActivity : AppCompatActivity() {
         val checkedRadioButton: RadioButton = findViewById(checkedID)
         val sex = checkedRadioButton.text.toString()
 
+        students.add(Student(name, address, mobileNumber, sex ))
 
+        val intent = Intent(this, Lab07OutputActivity::class.java)
+        intent.putExtra("students", students)
+        startActivity(intent)
+        Log.d("Lab07OutputActivity", "${students.toString()}")
     }
 }
