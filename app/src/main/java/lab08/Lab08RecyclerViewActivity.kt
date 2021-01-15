671d44cf81cd5b1d74a5cd584c5b1bbd40d4a502
mahatodaya <mahatodayaram1@gmail.com>
2021-01-15 21:55:32 +0545

    Commit for lab 8 exercise :

---
+++ app/src/main/java/lab08/Lab08RecyclerViewActivity.kt
@@ -0,0 +1,12 @@
+package lab08
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lab01.R
+
+class Lab08RecyclerViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lab08_recycler_view)
+    }
+}
