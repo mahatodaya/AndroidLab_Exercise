99fd6202880bab4acd024cab962bb3453f7cda3e
mahatodaya <mahatodayaram1@gmail.com>
2021-01-15 22:23:12 +0545

    3. Student Model data class was made.

---
+++ app/src/main/java/lab08/StudentModel.kt
@@ -0,0 +1,37 @@
+package lab08
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class StudentModel(
+    val image: String? = null,
+    val name: String? = null,
+    val address: String? = null
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(image)
+        parcel.writeString(name)
+        parcel.writeString(address)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<StudentModel> {
+        override fun createFromParcel(parcel: Parcel): StudentModel {
+            return StudentModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<StudentModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
